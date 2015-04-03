013a8ad62003de24a059f107ad9373c8ed5859b5
Hawken Rives <hawkrives@gmail.com>
2015-04-03 16:05:14 -0500

    Added ability for run_command to return a status code

--- _scripts/run_command.py
+++ _scripts/run_command.py
@@ -1,22 +1,22 @@
 import sys
 import subprocess
 
-def run(*args, **kwargs):
-	if ('input' in kwargs) and (type(kwargs['input']) is not bytes):
-		kwargs['input'] = bytes(kwargs['input'], encoding='utf-8')
+def run(*args, status=True, **kwargs):
+	# if ('input' in kwargs) and (type(kwargs['input']) is not bytes):
+	# 	kwargs['input'] = bytes(kwargs['input'], encoding='utf-8')
 
-	bytes_content = b''
-	result = ''
+	# bytes_content = b''
+	# result = ''
 
 	try:
-		bytes_content = subprocess.check_output(*args, stderr=subprocess.STDOUT, **kwargs)
+		result = (0, subprocess.check_output(*args, stderr=subprocess.STDOUT, universal_newlines=True, **kwargs))
 	except subprocess.CalledProcessError as err:
-		bytes_content = err.output
+		result = (1, err.output)
 	except subprocess.TimeoutExpired as err:
-		bytes_content = err.output
+		result = (1, err.output)
 	except FileNotFoundError as err:
-		result = repr(err)
+		result = (1, repr(err))
 
-	result = bytes.decode(bytes_content, encoding='utf-8') if bytes_content else result
+	# result = bytes.decode(bytes_content, encoding='utf-8') if bytes_content else result
 
-	return result
+	return result if status else result[1]

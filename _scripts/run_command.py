a5144148b339427d16708ae747fecd88f331d6c3
Hawken Rives <hawkrives@gmail.com>
2015-04-13 14:27:31 -0500

    Update and clean out run_command

--- _scripts/run_command.py
+++ _scripts/run_command.py
@@ -2,21 +2,21 @@ import sys
 import subprocess
 
 def run(*args, status=True, **kwargs):
-	# if ('input' in kwargs) and (type(kwargs['input']) is not bytes):
-	# 	kwargs['input'] = bytes(kwargs['input'], encoding='utf-8')
-
-	# bytes_content = b''
-	# result = ''
-
 	try:
-		result = (0, subprocess.check_output(*args, stderr=subprocess.STDOUT, universal_newlines=True, **kwargs))
+		result = (0, subprocess.check_output(
+			*args,
+			stderr=subprocess.STDOUT,
+			universal_newlines=True,
+			**kwargs))
 	except subprocess.CalledProcessError as err:
 		result = (1, err.output)
 	except subprocess.TimeoutExpired as err:
 		result = (1, err.output)
 	except FileNotFoundError as err:
 		result = (1, repr(err))
+	except ProcessLookupError as err:
+		result = (1, repr(err))
 
-	# result = bytes.decode(bytes_content, encoding='utf-8') if bytes_content else result
+	# print(result)
 
 	return result if status else result[1]

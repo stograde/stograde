52f1ce1719184b86b315bdc61d2c9fe9d1b994ce
Hawken Rives <hawkrives@gmail.com>
2016-02-26 12:39:22 -0600

    remove run_file

--- lib/run.py
+++ lib/run.py
@@ -39,10 +39,6 @@ def run_command(*args, status=True, **kwargs):
     return (code, result) if status else result
 
 
-def run_file(filePath, input='', **kwargs):
-    return run_command([filePath], input=input, timeout=4, **kwargs)
-
-
 if __name__ == '__main__':
     filePath = sys.argv[1]
-    print(run_file(filePath))
+    print(run_command([filePath]))

5f6d56bba006522820ce8f3345b6ee8e481f2512
Hawken Rives <hawkrives@gmail.com>
2016-02-27 17:30:50 -0600

    make some errors return better information

--- lib/run.py
+++ lib/run.py
@@ -20,23 +20,23 @@ def run_command(cmd, *args, status=True, stdout=PIPE, input=None, timeout=None, 
             **kwargs)
 
     except CalledProcessError as err:
-        status = 'process error'
-        result = err.output
+        status = 'called process error'
+        result = err.output if err.output else str(err)
 
     except TimeoutExpired as err:
         status = 'timed out after %s seconds' % timeout
-        result = err.output
+        result = err.output if err.output else str(err)
 
     except FileNotFoundError as err:
         status = 'not found'
-        result = repr(err)
+        result = str(err)
 
     except ProcessLookupError as err:
         try:
             status, result = run_command(*args, status=status, **kwargs)
         except:
-            status = 'process error'
-            result = repr(err)
+            status = 'process lookup error'
+            result = str(err)
 
     try:
         if type(result) is not str:

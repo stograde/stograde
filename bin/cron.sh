f1ff672a359d2b6a2c924455d94f9e987fef9093
Hawken Rives <hawkrives@gmail.com>
2016-04-05 12:22:13 -0500

    add cron script for emailing allen and myself

---
+++ bin/cron.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#TABLE=$(./update.py -w8 --all --no-progress)
+
+echo To: rives@stolaf.edu, allen@stolaf.edu
+echo Subject: Daily CS251 Homework Submission Update
+echo From: rives@stolaf.edu
+
+./update.py -w8 --all --no-progress

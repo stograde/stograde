7f373edfe1b4f60ccee9f2664a74f8e70b2e1fb7
Hawken Rives <hawkrives@gmail.com>
2016-04-19 21:24:35 -0500

    attempt #1 of html emails

--- bin/cron.sh
+++ bin/cron.sh
@@ -5,9 +5,28 @@ git stash
 git pull --rebase
 
 {
-	echo To: rives@stolaf.edu
-	echo Subject: Daily CS251 Homework Submission Update
+	boundary='BLAH.BLAH/hawken.mackay.riv.es'
+	subject='Daily CS251 Homework Submission Update'
 	echo From: rives@stolaf.edu
+	echo To: rives@stolaf.edu
+	echo Subject: "$subject"
+	echo MIME-Version: 1.0
+	echo Content-Type: multipart/alternative;
+	echo
+	echo "$boundary"
+	echo Content-Type: text/html
 
-	./update.py -w8 --all --no-progress
+	TABLE=$(./update.py -w8 --all --no-progress)
+
+	echo "<html>"
+	echo "<head>"
+	echo "<title>$subject</title>"
+	echo "</head>"
+	echo "<body>"
+	echo "<pre>"
+	echo "$TABLE"
+	echo "</pre>"
+	echo "</body>"
+	echo "</html>"
+	echo "--all--$boundary"
 } | sendmail -t

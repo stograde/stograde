eb53f3082bb2a230d9fa5f877217626f8a2b8245
Hawken Rives <hawkrives@gmail.com>
2016-04-19 21:05:51 -0500

    tweak the cron job a bit

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#TABLE=$(./update.py -w8 --all --no-progress)
+git add .
+git stash
+git pull --rebase
 
-echo To: rives@stolaf.edu, allen@stolaf.edu
-echo Subject: Daily CS251 Homework Submission Update
-echo From: rives@stolaf.edu
+{
+	echo To: rives@stolaf.edu, allen@stolaf.edu
+	echo Subject: Daily CS251 Homework Submission Update
+	echo From: rives@stolaf.edu
 
-./update.py -w8 --all --no-progress
+	./update.py -w8 --all --no-progress
+} | sendmail -t

0f32b73e6736ba2e96486aa782efc3e7d7b86f14
Hawken Rives <hawkrives@gmail.com>
2016-09-19 18:23:10 +0900

    add a todo comment

--- cs251tk/lib/find_unmerged_branches_in_cwd.py
+++ cs251tk/lib/find_unmerged_branches_in_cwd.py
@@ -3,6 +3,7 @@
 from .run import run
 
 
+# TODO: This is a bit slow. Look for a faster way to check.
 def find_unmerged_branches_in_cwd():
     '''Check for unmerged branches in the current repository'''
     _, unmerged_branches = run(['git', 'branch', '-a', '--no-merged', 'master'])

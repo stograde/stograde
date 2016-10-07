45764c22e4cf5186aa07475583269de67af230a2
Hawken Rives <hawkrives@gmail.com>
2016-10-08 01:22:24 +0900

    allow calling process_student

--- cs251tk/toolkit/__init__.py
+++ cs251tk/toolkit/__init__.py
@@ -3,5 +3,6 @@ from .config import load_config, save_config
 from .get_students import get_students
 from .gist import post_gist
 from .progress_bar import progress_bar
+from .process_student import process_student
 from .save_recordings import save_recordings, gist_recordings
 from .tabulate import tabulate, asciiify

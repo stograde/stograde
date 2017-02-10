3241887f2da75eac8de6424f505a5b59c7741576
Hawken Rives <hawkrives@gmail.com>
2017-02-10 10:39:03 -0600

    add more tests for run()

--- cs251tk/common/test_run.py
+++ cs251tk/common/test_run.py
@@ -5,3 +5,21 @@ def test_run():
     status, result = run(['echo', 'hi'])
     assert status == 'success'
     assert result == 'hi\n'
+
+
+def test_run_stdin():
+    status, result = run(['cat'], input_data=b'hello')
+    assert status == 'success'
+    assert result == 'hello'
+
+
+def test_run_timeout():
+    status, result = run(['sleep', '1'], timeout=0.5)
+    assert status == 'timed out after 0.5 seconds'
+    assert result == "Command '['sleep', '1']' timed out after 0.5 seconds"
+
+
+def test_run_not_found():
+    status, result = run(['notfound'])
+    assert status == 'not found'
+    assert result == ''

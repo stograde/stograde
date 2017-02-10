bbb40ddf6ac9f59452bc84bd87acc2853469081b
Hawken Rives <hawkrives@gmail.com>
2017-02-09 19:19:46 -0600

    remove a redundant variable in find_unmerged_branches

--- cs251tk/common/find_unmerged_branches_in_cwd.py
+++ cs251tk/common/find_unmerged_branches_in_cwd.py
@@ -5,7 +5,6 @@ from .run import run
 def find_unmerged_branches_in_cwd():
     """Check for unmerged branches in the current repository"""
     _, unmerged_branches = run(['git', 'branch', '-a', '--no-merged', 'master'])
-    unmerged_branches = [s.strip()
-                         for s in unmerged_branches.split('\n')
-                         if s.strip()]
-    return unmerged_branches
+    return [s.strip()
+            for s in unmerged_branches.split('\n')
+            if s.strip()]

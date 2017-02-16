2edf5584733e54b18dce7af16078956a2b3e8884
Hawken Rives <hawkrives@gmail.com>
2017-02-16 09:28:36 -0600

    update the version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='cs251tk',
-    version='2.1.8',
+    version='2.1.10',
     description='The CS251 (Software Design) Toolkit',
     author='Hawken Rives',
     author_email='hawkrives@gmail.com',

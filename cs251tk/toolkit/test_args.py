b12b8b4dc8f704eb1415c41299ab84a9f25d87b4
Hawken Rives <hawkrives@gmail.com>
2017-02-16 17:38:01 -0600

    fix test_args tests

--- cs251tk/toolkit/test_args.py
+++ cs251tk/toolkit/test_args.py
@@ -1,8 +1,13 @@
-from .args import get_args, massage_args
+from .args import (
+    build_argparser,
+    get_students_from_args,
+    get_assignments_from_args,
+    compute_stogit_url,
+)
 
 
 def args(arglist):
-    return vars(get_args().parse_args(args=arglist))
+    return vars(build_argparser().parse_args(args=arglist))
 
 
 students = {
@@ -13,32 +18,28 @@ students = {
 
 
 def test_all():
-    # check that --all sets `all` to true
-    assert massage_args(args([]), students)['all'] == False
-    assert massage_args(args(['--all']), students)['all'] == True
-
     # check that --all includes all students
-    assert massage_args(args(['--all']), students)['students'] == students['my'] + students['section-a'] + students['section-b']
+    assert get_students_from_args(**args(['--all']), _all_students=students) == students['my'] + students['section-a'] + students['section-b']
 
 
 def test_students():
     # multiple sets of --students should wind up as one flattened list
-    assert massage_args(args(['--students', 'a', 'b', '--students', 'c']), students)['students'] == ['a', 'b', 'c']
+    assert get_students_from_args(**args(['--students', 'a', 'b', '--students', 'c']), _all_students=students) == ['a', 'b', 'c']
 
     # it should return a sorted list of student names
-    assert massage_args(args(['--students', 'c', 'b', '--students', 'a']), students)['students'] == ['a', 'b', 'c']
+    assert get_students_from_args(**args(['--students', 'c', 'b', '--students', 'a']), _all_students=students) == ['a', 'b', 'c']
 
     # multiple occurences of the same student should be removed
-    assert massage_args(args(['--students', 'a', 'a', '--students', 'a']), students)['students'] == ['a']
+    assert get_students_from_args(**args(['--students', 'a', 'a', '--students', 'a']), _all_students=students) == ['a']
 
     # if no students are given, it should default to the "my" section
-    assert massage_args(args([]), students)['section'] == ['my']
+    assert get_students_from_args(**args([]), _all_students=students) == students['my']
 
 
 def test_section():
     # "--section $name" should return the students for that section
-    assert massage_args(args(['--section', 'a']), students)['students'] == students['section-a']
+    assert get_students_from_args(**args(['--section', 'a']), _all_students=students) == students['section-a']
 
 
 def test_record():
-    assert massage_args(args(['--record', 'hw4']), students)['record'] == ['hw4']
+    assert get_assignments_from_args(**args(['--record', 'hw4'])) == ['hw4']

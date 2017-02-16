9edf14e07ca47fc5a17bd423d357dc82c31b95a8
Hawken Rives <hawkrives@gmail.com>
2017-02-16 15:17:48 -0600

    use capsys instead of custom stdout capture

--- integration_tests/test_e2e.py
+++ integration_tests/test_e2e.py
@@ -11,34 +11,19 @@ from cs251tk.cli.cs251tk import main
 _dir = os.path.dirname(os.path.realpath(__file__))
 
 
-@pytest.fixture(scope='function')
-def setup(datafiles):
-    # back up stdout/stderr and replace with testable version
-    stdout = StringIO()
-    stderr = StringIO()
-
-    # catch exceptions so that we always clean up
-    with redirect_stdout(stdout):
-        with redirect_stderr(stderr):
-            try:
-                yield stdout, stderr
-            except Exception:
-                pass
-
-
 @pytest.mark.datafiles(os.path.join(_dir, 'fixtures', 'two_students_hw1'))
-def test_cs251tk_table(setup, datafiles):
+def test_cs251tk_table(datafiles, capsys):
     os.chdir(str(datafiles))
 
-    stdout, stderr = setup
-    sys.argv = [sys.argv[0]] + ['--no-update', '--no-check', '--skip-update-check']
+    argv = sys.argv
+    sys.argv = [argv[0]] + ['--no-update', '--no-check', '--skip-update-check']
 
     try:
         main()
     except SystemExit:
         pass
 
-    out = stdout.getvalue()
+    out, err = capsys.readouterr()
 
     assert out == textwrap.dedent("""
     USER      | 1 | 1
@@ -46,3 +31,5 @@ def test_cs251tk_table(setup, datafiles):
     rives     | - | -
     student2  | 1 | 1
     """)
+
+    sys.argv = argv

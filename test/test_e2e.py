6812546b59a520de93ad65a90a78d479adfe8a75
Hawken Rives <hawkrives@gmail.com>
2017-02-16 12:42:50 -0600

    mock stdout/stderr

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -1,37 +1,69 @@
+import textwrap
+import pytest
+import locale
 import sys
 import os
 import re
-from textwrap import dedent
+from io import StringIO
 from cs251tk.cli.cs251tk import main
 
 
-def test_cs251tk_version(capsys):
+@pytest.fixture(scope='function', autouse=True)
+def setup():
+    # move the process into the test/ dir
+    os.chdir("./test")
+
+    # back up stdout/stderr and replace with testable version
+    _stdout = sys.stdout
+    _stderr = sys.stderr
+    sys.stdout = StringIO()
+    sys.stderr = StringIO()
+
+    # catch exceptions so that we always clean up
+    try:
+        yield sys.stdout, sys.stderr
+    except Exception:
+        pass
+
+    # replace stdout/stderr with the actual versions
+    sys.stdout.close()
+    sys.stderr.close()
+    sys.stdout = _stdout
+    sys.stderr = _stderr
+
+    # return to the previous directory
+    os.chdir("..")
+
+
+def test_cs251tk_version(setup):
+    stdout, stderr = setup
     sys.argv = [sys.argv[0]] + ['-v']
-    os.chdir("test")
 
     try:
         main()
     except SystemExit:
         pass
 
-    out, err = capsys.readouterr()
-
+    out = stdout.getvalue()
     out = out.strip()
     regex = re.compile(r"version \d+[.]\d+[.]\d+")
 
     assert regex.match(out)
 
-    os.chdir("..")
 
-
-def test_cs251tk_table(capsys):
+def test_cs251tk_table(setup):
+    stdout, stderr = setup
     sys.argv = [sys.argv[0]] + ['-n']
-    os.chdir("test")
 
-    main()
+    try:
+        main()
+    except SystemExit:
+        pass
 
-    out, err = capsys.readouterr()
-    assert out == dedent("""
+    out = stdout.getvalue()
+    err = stderr.getvalue()
+
+    assert out == textwrap.dedent("""
     USER      │ 1 │ 1
     ──────────┼───┼──
     [1mrives   [0m  │ ─ │ ─
@@ -41,5 +73,3 @@ def test_cs251tk_table(capsys):
     assert "[  ] rives, student2" in err
     assert "[· ] student2" in err
     assert "[··]" in err
-
-    os.chdir("..")

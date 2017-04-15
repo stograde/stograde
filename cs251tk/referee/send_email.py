f7236b7232ee6e7046800fb5c6ef3e2291c003cc
Hawken Rives <hawkrives@gmail.com>
2017-04-15 12:15:33 -0500

    add username and password auth to send_email

--- cs251tk/referee/send_email.py
+++ cs251tk/referee/send_email.py
@@ -2,6 +2,14 @@ import smtplib
 
 
 def send_email(msg):
+    username = os.getenv('CS251TK_EMAIL_USERNAME', None)
+    if not username:
+        raise Exception('Missing $CS251TK_EMAIL_USERNAME')
+    password = os.getenv('CS251TK_EMAIL_PASSWORD', None)
+    if not password:
+        raise Exception('Missing $CS251TK_EMAIL_PASSWORD')
+
     # Send the message via our own SMTP server.
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as s:
+        s.login(username, password)
         s.send_message(msg)

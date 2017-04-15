7b289121d0bf060a9381ae11f4a062aceb088904
Kristofer Rye <kristofer.rye@gmail.com>
2017-04-15 02:29:54 -0500

    Improve email setup and use multipart magic

--- cs251tk/referee/emailify.py
+++ cs251tk/referee/emailify.py
@@ -1,27 +1,43 @@
-from cs251tk.formatters import format_collected_data, markdown
+from cs251tk.formatters import format_collected_data, html, markdown
+from cs251tk.common import group_by
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
 def emailify(recordings, name, to, debug):
-    grouped = format_collected_data(recordings,
-                                    group_by='student',
-                                    formatter=markdown,
-                                    debug=debug)
+    fancy = format_collected_data(recordings,
+                                  group_by='student',
+                                  formatter=html,
+                                  debug=debug)
+
+    plaintext = format_collected_data(recordings,
+                                      group_by='student',
+                                      formatter=markdown,
+                                      debug=debug)
+
+    grouped = dict(group_by(recordings, lambda s: s['student']))
 
     # `grouped` is a dictionary, from username to list of assignment results
     if not grouped:
         raise Exception('No students found to create an email!')
     if len(grouped) > 1:
         raise Exception('More than one student found when creating an email!')
-    assignments = list(grouped.values())[0]
 
-    body = '\n'.join([r['content'] for r in assignments])
-    msg = MIMEText(body)
+    fancy_body = '\n'.join([r['content'] for r in list(fancy.values())[0]])
+    plaintext_body = '\n'.join([r['content'] for r in list(plaintext.values())[0]])
+    print(fancy_body)
+    print(plaintext_body)
+
+    msg = MIMEMultipart('alternative')
     msg['To'] = '{} <{}>'.format(name, to)
     msg['From'] = 'cs251-tas@stolaf.edu'
-    msg['Subject'] = build_subject(assignments)
+    msg['Subject'] = build_subject(list(fancy.values())[0])
+
+    msg.attach(MIMEText(plaintext_body, 'plain'))
+    msg.attach(MIMEText(fancy_body, 'html'))
+
     return msg
 
 
 def build_subject(results):
-    return 'The ' + ', '.join([r['assignment'] for r in results]) + ' submission'
+    return '[referee] Results for ' + ', '.join([r['assignment'] for r in results]) + ' submission'
--- cs251tk/referee/send_email.py
+++ cs251tk/referee/send_email.py
@@ -3,5 +3,7 @@ import smtplib
 
 def send_email(msg):
     # Send the message via our own SMTP server.
-    with smtplib.SMTP('localhost') as s:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as s:
+        # s.starttls()
+        s.set_debuglevel(2)
         s.send_message(msg)

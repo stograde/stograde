863b12e503559b24de29407cd674d432bdcbbfc0
Kristofer Rye <kristofer.rye@gmail.com>
2017-04-15 09:01:12 -0500

    Remove somore more lines related to email

--- cs251tk/referee/send_email.py
+++ cs251tk/referee/send_email.py
@@ -4,6 +4,4 @@ import smtplib
 def send_email(msg):
     # Send the message via our own SMTP server.
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as s:
-        # s.starttls()
-        s.set_debuglevel(2)
         s.send_message(msg)

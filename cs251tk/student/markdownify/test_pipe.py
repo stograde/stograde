27fcf99d614098d75fbd0010edea7423003df5db
Hawken Rives <hawkrives@gmail.com>
2018-10-24 16:17:43 -0500

    continue replacing CreateFile with create_file

--- cs251tk/student/markdownify/test_pipe.py
+++ cs251tk/student/markdownify/test_pipe.py
@@ -2,16 +2,16 @@ from .pipe import pipe, expand_chunk, process_chunk
 
 
 def test_expand_chunk(fs):
-    fs.CreateFile('file1')
-    fs.CreateFile('file2')
+    fs.create_file('file1')
+    fs.create_file('file2')
 
     assert expand_chunk('ls') == ['ls']
     assert sorted(expand_chunk('file*')) == sorted(['file1', 'file2'])
 
 
 def test_process_chunk(fs):
-    fs.CreateFile('file1')
-    fs.CreateFile('file2')
+    fs.create_file('file1')
+    fs.create_file('file2')
 
     assert process_chunk('echo Hawken \n 26') == ['echo', 'Hawken', '26']
 

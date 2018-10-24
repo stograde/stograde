1e19fd0a004e3589a5934ed4f0323775e9611b68
Hawken Rives <hawkrives@gmail.com>
2018-10-24 16:17:06 -0500

    continue removing deprecated test calls

--- cs251tk/common/test_dirsize.py
+++ cs251tk/common/test_dirsize.py
@@ -5,11 +5,11 @@ from .dirsize import dirsize
 def test_chdir(fs):
     os_module = fake_filesystem.FakeOsModule(fs)
 
-    fs.CreateDirectory('/folder')
-    fs.CreateFile('/folder/file1', contents='0123456789')
-    fs.CreateFile('/folder/file2', contents='0123456789')
-    fs.CreateFile('/folder/file3', contents='0123456789')
-    fs.CreateFile('/folder/file4', contents='0123456789')
-    fs.CreateFile('/folder/file5', contents='0123456789')
+    fs.create_dir('/folder')
+    fs.create_file('/folder/file1', contents='0123456789')
+    fs.create_file('/folder/file2', contents='0123456789')
+    fs.create_file('/folder/file3', contents='0123456789')
+    fs.create_file('/folder/file4', contents='0123456789')
+    fs.create_file('/folder/file5', contents='0123456789')
 
     print(dirsize('/folder'))

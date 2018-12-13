398ceb2cd2bbf508fea8eea2d64c2a1b95a1f221
Max Narvaez <narvae1@stolaf.edu>
2018-12-13 15:11:50 -0600

    Revert "Finish removing .idea directory"
    This reverts commit dda2d1c23ed50e6593e544b028bfc8bf6bc90f32.

--- cs251tk/specs/__init__.py
+++ cs251tk/specs/__init__.py
@@ -1,4 +1,4 @@
 from .load import load_all_specs
 from .load import load_some_specs
 from .util import get_filenames
-from .util import check_dependencies, check_architecture
+from .util import check_dependencies
--- cs251tk/specs/util.py
+++ cs251tk/specs/util.py
@@ -1,8 +1,6 @@
 import os
 from logging import warning
 
-from ..common import run
-
 
 def get_filenames(spec):
     """returns the list of files from an assignment spec"""
@@ -15,8 +13,3 @@ def check_dependencies(spec):
             os.stat(filepath)
         except FileNotFoundError:
             warning('spec {}: required file "{}" could not be found'.format(spec['assignment'], filepath))
-
-
-def check_architecture(spec):
-    _, res, _ = run(['uname', '-a'])
-    return res.split(" ")[-2]

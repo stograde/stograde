4079cf8814918f8393259133234e69d2a041019c
Hawken Rives <hawkrives@gmail.com>
2019-02-16 23:56:56 -0600

    Update cs251tk/student/markdownify/process_file.py
    Co-Authored-By: maxnz <mnmax.narvaez3@gmail.com>

--- cs251tk/student/markdownify/process_file.py
+++ cs251tk/student/markdownify/process_file.py
@@ -30,7 +30,7 @@ def get_file(filename, results, options):
 
 def compile_file(filename, steps, results, supporting_dir, basedir, web, student):
     server_path = ' '.join([
-        '-o "{}/server/server_file" '.format(basedir),
+        '-o "{}/server/server_file"'.format(basedir),
         '"{}/data/supporting/sd_fun.h" '.format(basedir),
         '"{}/data/supporting/sd_fun.o" '.format(basedir),
         '"{}/data/supporting/str_util.o"'.format(basedir)

b54f6723aa16ab9611c064ad680bf6c0d7200477
Hawken Rives <hawkrives@gmail.com>
2019-03-13 13:31:15 -0500

    v2.7.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='cs251tk',
-    version='2.6.1',
+    version='2.7.0',
     description='The CS251 (Software Design) Toolkit',
     author='Hawken Rives',
     author_email='hawkrives@gmail.com',

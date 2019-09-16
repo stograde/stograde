335e2857128a02de6c9a7708285c00eb9f7474c9
Hawken Rives <hawkrives@fastmail.fm>
2019-09-16 08:34:30 -0500

    v2.8.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='stograde',
-    version='2.8.1',
+    version='2.8.2',
     description='The StoGrade Toolkit',
     author='Hawken Rives',
     author_email='hawkrives@gmail.com',

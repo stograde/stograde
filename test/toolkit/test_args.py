6ea4facac0b1503f32e95396e3867e232f5ac70a
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-02-12 14:17:27 -0600

    Update test

--- test/toolkit/test_args.py
+++ test/toolkit/test_args.py
@@ -48,16 +48,16 @@ def test_record():
 
 def test_stogit_url_computation():
     assert compute_stogit_url(stogit=None, course='sd', _now=datetime.date(2017, 1, 31)) \
-        == 'git@stogit.cs.stolaf.edu:sd-s17'
+        == 'git@stogit.cs.stolaf.edu:sd/s17'
 
     assert compute_stogit_url(stogit=None, course='sd', _now=datetime.date(2016, 9, 15)) \
-        == 'git@stogit.cs.stolaf.edu:sd-f16'
+        == 'git@stogit.cs.stolaf.edu:sd/f16'
 
     assert compute_stogit_url(stogit=None, course='sd', _now=datetime.date(2016, 4, 15)) \
-        == 'git@stogit.cs.stolaf.edu:sd-s16'
+        == 'git@stogit.cs.stolaf.edu:sd/s16'
 
     assert compute_stogit_url(stogit='blah', course='sd', _now=datetime.date.today()) \
         == 'blah'
 
     assert compute_stogit_url(stogit=None, course='hd', _now=datetime.date(2016, 4, 15)) \
-        == 'git@stogit.cs.stolaf.edu:hd-s16'
+        == 'git@stogit.cs.stolaf.edu:hd/s16'

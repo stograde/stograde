a12185ac2c4ded7cf90e31abc10711ff70618267
Hawken Rives <hawkrives@fastmail.fm>
2020-02-16 14:35:39 -0600

    v3.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='stograde',
-    version='2.9.2',
+    version='3.0.0',
     description='The StoGrade Toolkit',
     author='Hawken Rives',
     author_email='hawkrives@gmail.com',

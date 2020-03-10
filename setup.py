be39ed740da720d58f9731e9ce071bff28d7ec2b
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-03-10 15:16:28 -0500

    v3.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info < (3, 5):
 
 setup(
     name='stograde',
-    version='3.0.0',
+    version='3.0.1',
     description='The StoGrade Toolkit',
     author='Hawken Rives',
     author_email='hawkrives@gmail.com',

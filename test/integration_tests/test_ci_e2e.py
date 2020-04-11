a22aef46651fc0a998e62a9399dab20e126652a7
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-04-11 13:28:11 -0500

    Add test for CI failing due to compile error

--- test/integration_tests/test_ci_e2e.py
+++ test/integration_tests/test_ci_e2e.py
@@ -6,7 +6,6 @@ import os
 import pytest
 
 from stograde.toolkit.__main__ import main
-from stograde.toolkit.tabulate import asciiify
 
 _dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -34,6 +33,35 @@ def test_stograde_ci_passing(datafiles, capsys):
                                   "USER      | 1 |  | \n"
                                   "----------+---+--+-\n"
                                   "student2  | 1 |  | \n\n")
+
+    sys.argv = argv
+
+    shutil.rmtree(os.path.join(datafiles, 'data'))
+
+
+@pytest.mark.datafiles(os.path.join(_dir, 'fixtures', 'two_students_hw1', 'students', 'student1'))
+def test_stograde_ci_passing_stogradeignore(datafiles, capsys):
+    os.chdir(str(datafiles))
+
+    shutil.copytree(os.path.join(_dir, 'fixtures', 'two_students_hw1', 'data'), os.path.join(datafiles, 'data'))
+
+    os.environ['CI_PROJECT_NAME'] = 'student1'
+    os.environ['CI_PROJECT_NAMESPACE'] = 'sd/s20'
+
+    argv = sys.argv
+    sys.argv = [argv[0]] + ['ci', '--skip-spec-update', '--skip-version-check', '--skip-dependency-check']
+
+    try:
+        main()
+    except SystemExit:
+        pass
+
+    out, err = capsys.readouterr()
+
+    assert out == textwrap.dedent("\n"
+                                  "USER      | 1 |  | \n"
+                                  "----------+---+--+-\n"
+                                  "student1  | 1 |  | \n\n")
 
     sys.argv = argv
 
@@ -81,13 +109,13 @@ def test_stograde_ci_failing(datafiles, capsys, caplog):
     shutil.rmtree(os.path.join(datafiles, 'data'))
 
 
-@pytest.mark.datafiles(os.path.join(_dir, 'fixtures', 'two_students_hw1', 'students', 'student1'))
-def test_stograde_ci_passing_stogradeignore(datafiles, capsys):
+@pytest.mark.datafiles(os.path.join(_dir, 'fixtures', 'two_students_hw1', 'students', 'student3'))
+def test_stograde_ci_failing_compile(datafiles, capsys, caplog):
     os.chdir(str(datafiles))
 
     shutil.copytree(os.path.join(_dir, 'fixtures', 'two_students_hw1', 'data'), os.path.join(datafiles, 'data'))
 
-    os.environ['CI_PROJECT_NAME'] = 'student1'
+    os.environ['CI_PROJECT_NAME'] = 'student3'
     os.environ['CI_PROJECT_NAMESPACE'] = 'sd/s20'
 
     argv = sys.argv
@@ -100,11 +128,25 @@ def test_stograde_ci_passing_stogradeignore(datafiles, capsys):
 
     out, err = capsys.readouterr()
 
+    log_messages = [bytes(log.msg, 'utf-8') for log in caplog.records]
+
+    assert len(log_messages) == 1
+
+    assert bytes("hw1: File hello.cpp compile error:\n\n\t"
+                 "./hello.cpp: In function ‘int main()’:\n\t"
+                 "./hello.cpp:11:12: error: expected ‘}’ at end of input\n\t"
+                 "    return 0;\n\t"
+                 "            ^\n\t", 'utf-8') in log_messages
+
+    for log in caplog.records:
+        assert log.levelname == 'ERROR'
+
     assert out == textwrap.dedent("\n"
                                   "USER      | 1 |  | \n"
                                   "----------+---+--+-\n"
-                                  "student1  | 1 |  | \n\n")
+                                  "student3  | 1 |  | \n\n")
 
     sys.argv = argv
 
     shutil.rmtree(os.path.join(datafiles, 'data'))
+

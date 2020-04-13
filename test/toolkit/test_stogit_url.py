e80dbba0aaabf4f90b6ef27a922635bec84cd1eb
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-04-13 09:38:45 -0500

    Add tests for get_course_from_specs()

--- test/toolkit/test_stogit_url.py
+++ test/toolkit/test_stogit_url.py
@@ -1,6 +1,8 @@
 import datetime
+import shutil
 
-from stograde.toolkit.stogit_url import compute_stogit_url
+from stograde.common import run
+from stograde.toolkit.stogit_url import compute_stogit_url, get_course_from_specs
 
 
 def test_stogit_url_computation():
@@ -18,3 +20,23 @@ def test_stogit_url_computation():
 
     assert compute_stogit_url(stogit='', course='hd', _now=datetime.date(2016, 4, 15)) \
         == 'git@stogit.cs.stolaf.edu:hd/s16'
+
+
+def test_get_course_from_specs_success():
+    shutil.rmtree('data', ignore_errors=True)
+    run(['git', 'clone', 'https://github.com/StoDevX/cs251-specs.git', 'data'])
+    course = get_course_from_specs()
+    assert course == 'sd'
+    shutil.rmtree('data')
+
+
+def test_get_course_from_specs_failure(capsys):
+    shutil.rmtree('data', ignore_errors=True)
+    try:
+        get_course_from_specs()
+    except SystemExit:
+        pass
+
+    _, err = capsys.readouterr()
+
+    assert err == 'Cannot determine course from specs\n'

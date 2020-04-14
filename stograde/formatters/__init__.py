47fffa2cbffe2c25cf538e86a3be527e421dfea9
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-04-14 16:26:20 -0500

    Remove yaml formatter (unused after removal of caching)

--- stograde/formatters/__init__.py
+++ stograde/formatters/__init__.py
@@ -2,4 +2,3 @@ from .base import format_collected_data
 from .html import format_assignment_html as html
 from .markdown import format_assignment_markdown as markdown
 from .truncate import truncate
-from .yaml import format_assignment_yaml as yaml

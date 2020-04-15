a7c1a232acdbf13c1aebe9b083f35387d3b645e8
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-04-15 16:31:01 -0500

    Add tests for pull

---
+++ test/student/test_pull.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+from stograde.student import pull, clone_url
+
+
+def test_pull_success(tmpdir, caplog):
+    with tmpdir.as_cwd():
+        clone_url('https://github.com/StoDevX/cs251-specs.git')
+        with caplog.at_level(logging.DEBUG):
+            pull('cs251-specs')
+
+    log_messages = [log.msg for log in caplog.records]
+
+    assert len(log_messages) == 1
+
+    assert log_messages == ["Pulling cs251-specs's repository"]
+
+
+def test_pull_fail(tmpdir, capsys):
+    with tmpdir.as_cwd():
+        os.makedirs('not_a_git_repo')
+        pull('not_a_git_repo')
+
+    _, err = capsys.readouterr()
+
+    assert err == 'Student directory not_a_git_repo is not a git repository\n' \
+                  'Try running "stograde repo reclone"\n'
+
+    pass

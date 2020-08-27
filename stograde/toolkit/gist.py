1ad1210361ad052ce1d7f7b75d56c94ef9c03df3
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-08-27 14:19:32 -0500

    Use github personal access token instead of password Using a password will be deprecated in November 2020 according to https://docs.github.com/en/rest/overview/other-authentication-methods#via-username-and-password

--- stograde/toolkit/gist.py
+++ stograde/toolkit/gist.py
@@ -2,21 +2,21 @@
 import getpass
 import json
 import requests
+
 __all__ = ['post_gist']
 
 
 def get_auth():
     """Get the user's credentials"""
     username = input('Github username: ')
-    password = getpass.getpass('Github password: ')
-    return username, password
+    token = getpass.getpass('Github personal access token: ')
+    return username, token
 
 
 def post_gist(description, files):
     """Post a gist of the analysis"""
-    username, password = get_auth()
+    username, token = get_auth()
     sess = requests.Session()
-    sess.auth = (username, password)
 
     params = {
         'description': description,
@@ -25,9 +25,11 @@ def post_gist(description, files):
     }
 
     headers = {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': 'token {}'.format(token),
         'Content-Type': 'application/json',
-        'Accept': '*/*',
         'User-Agent': 'stolaf-cs-toolkit/v1',
+        'X-Github-Username': username,
     }
 
     req = sess.post('https://api.github.com/gists',

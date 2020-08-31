413baeb9fb82d89a641ea2f0a1d0ec404927ffb8
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-08-30 21:51:48 -0500

    Make group_by generic

--- stograde/common/group_by.py
+++ stograde/common/group_by.py
@@ -1,15 +1,12 @@
 from itertools import groupby
 from types import LambdaType
-from typing import Any, Generator, List, Tuple, TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from ..process_assignment.record_result import RecordResult
+from typing import Any, Generator, Iterable, List, Tuple
 
 __all__ = ['group_by']
 
 
-def group_by(iterable: List['RecordResult'],
-             predicate: LambdaType) -> Generator[Tuple[Any, List['RecordResult']], None, None]:
+def group_by(iterable: Iterable,
+             predicate: LambdaType) -> Generator[Tuple[Any, List], None, None]:
     """Group an iterable by a predicate function"""
     sorted_iterable = sorted(iterable, key=predicate)
     grouped = groupby(sorted_iterable, key=predicate)

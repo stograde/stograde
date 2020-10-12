d12fcce8bdabe97f390ba0b2066b30eba9299d6b
Max Narvaez <mnmax.narvaez3@gmail.com>
2020-10-12 14:05:36 -0500

    Add comments about what is requiring specific dependencies

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ install_requires = [
     'natsort == 7.0.*',
     'appdirs == 1.4.*',
     'python-dateutil == 2.7.*',
-    'PyInquirer == 1.0.*',
-    # stograde drive
-    'google-api-python-client == 1.12.*',
-    'google-auth-oauthlib == 0.4.*',
+    'PyInquirer == 1.0.*',  # required by stograde web
+    'google-api-python-client == 1.12.*',  # required by stograde drive
+    'google-auth-oauthlib == 0.4.*',  # required by stograde drive
     'six >= 1.13.0',  # required by google-api-core
     'setuptools >= 40.3.0',  # required by google-auth
 ]
 
-if sys.version_info < (3, 7):
+# This allows us to use dataclasses in python 3.6
+if sys.version_info == (3, 6):
     install_requires.append('dataclasses >= 0.6')
 
 setup(

4d6b9a8ae18d063b5b35ed6d8377138ce273d511
Max Narvaez <mnmax.narvaez3@gmail.com>
2021-10-10 12:14:43 -0500

    Try setting output

--- script/docker-tags.bash
+++ script/docker-tags.bash
@@ -4,28 +4,28 @@ suffix="$1"
 
 if [[ $GITHUB_REF == "ref/heads/master" ]]
 then
-  deploy_targets="$deploy_targets $DOCKER_IMAGE:HEAD$suffix"
+  tags="$tags $DOCKER_IMAGE:HEAD$suffix"
 fi
 
 if [[ $GITHUB_REF =~ ref/heads/travis-.* ]]
 then
-  deploy_targets="$deploy_targets $DOCKER_IMAGE:$GITHUB_SHA$suffix"
+  tags="$tags $DOCKER_IMAGE:$GITHUB_SHA$suffix"
 fi
 
 if [[ $GITHUB_REF == "refs/heads/migrate-to-gh-actions" ]]
 then
-  deploy_targets="$deploy_targets $DOCKER_IMAGE:$GITHUB_SHA$suffix"
+  tags="$tags $DOCKER_IMAGE:$GITHUB_SHA$suffix"
 fi
 
 if [[ $GITHUB_REF =~ ref/tags/v.* ]]
 then
-  deploy_targets="$deploy_targets $DOCKER_IMAGE:$TRAVIS_TAG$suffix"
+  tags="$tags $DOCKER_IMAGE:$TRAVIS_TAG$suffix"
   if [[ "$(script/github-latest-release)" == "${GITHUB_REF#refs/tags/}" ]]
   then
-    deploy_targets="$deploy_targets $DOCKER_IMAGE:latest$suffix"
+    tags="$tags $DOCKER_IMAGE:latest$suffix"
   fi
 fi
 
-deploy_targets=${deploy_targets# }
+tags=${tags# }
 
-echo "$deploy_targets"
+echo "::set-output name=tags::$tags"

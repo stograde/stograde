8ea2ffd75c39d8271af178019d5083c23e2e53b7
Max Narvaez <mnmax.narvaez3@gmail.com>
2022-12-08 23:00:08 -0600

    Split tests

--- test/common/test_dirsize.py
+++ test/common/test_dirsize.py
@@ -13,5 +13,14 @@ def test_dirsize(fs):
 
     assert dirsize('/folder') == 50
 
+
+def test_dirsize_fail(fs):
+    fs.create_dir('/folder')
+    fs.create_file('/folder/file1', contents='0123456789')
+    fs.create_file('/folder/file2', contents='0123456789')
+    fs.create_file('/folder/file3', contents='0123456789')
+    fs.create_file('/folder/file4', contents='0123456789')
+    fs.create_file('/folder/file5', contents='0123456789')
+
     with mock.patch('os.path.getsize', side_effect=OSError('An error was thrown')):
         assert dirsize('/folder') == 0

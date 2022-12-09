15bebbb951eee1fffc7c05c6c7bf70e33d687963
Max Narvaez <mnmax.narvaez3@gmail.com>
2022-12-08 23:15:22 -0600

    Finish test fix

--- test/common/test_dirsize.py
+++ test/common/test_dirsize.py
@@ -1,3 +1,4 @@
+import os.path
 from unittest import mock
 
 from stograde.common.dirsize import dirsize
@@ -16,11 +17,7 @@ def test_dirsize(fs):
 
 def test_dirsize_fail(fs):
     fs.create_dir('/folder')
-    fs.create_file('/folder/file1', contents='0123456789')
-    fs.create_file('/folder/file2', contents='0123456789')
-    fs.create_file('/folder/file3', contents='0123456789')
-    fs.create_file('/folder/file4', contents='0123456789')
-    fs.create_file('/folder/file5', contents='0123456789')
 
-    with mock.patch('os.path.getsize', side_effect=OSError('An error was thrown')):
-        assert dirsize('/folder2') == 0
+    assert not os.path.exists("/folder/subfolder")
+
+    assert dirsize('/folder/subfolder') == 0
